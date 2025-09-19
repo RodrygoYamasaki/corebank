ec83d4ff45ae11e1163e9dba5b297d9cbd952aeb
RodrygoYamasaki <rodrygo.yamasaki@gmail.com>
2025-09-19 16:42:02 -0300

    Initial project setup with Spring Boot

---
+++ src/test/java/br/com/fiap/corebank/CorebankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.corebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
