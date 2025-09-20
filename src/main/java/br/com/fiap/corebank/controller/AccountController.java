d497aa059ea1474094f122bc3aa63a390199ff81
RodrygoYamasaki <rodrygo.yamasaki@gmail.com>
2025-09-20 16:38:57 -0300

    Create AccountController, AccountRepository, and ValidationHandler

---
+++ src/main/java/br/com/fiap/corebank/controller/AccountController.java
@@ -0,0 +1,5 @@
+package br.com.fiap.corebank.controller;
+
+public class AccountController {
+    
+}
---
+++ src/main/java/br/com/fiap/corebank/repository/AccountRepository.java
@@ -0,0 +1,5 @@
+package br.com.fiap.corebank.repository;
+
+public class AccountRepository {
+    
+}
